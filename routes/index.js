063b62e9ae6e234b58d81d7c7effc421441fad8b
Jordan Buschman <jordan.buschman@me.com>
2014-12-18 18:27:32 -0800

    Adding routes/views

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
-var express = require('express');
+var express  = require('express');
+var passport = require('passport');
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+    res.render('index');
+});
+
+router.get('/login', function(req, res) {
+    res.render('login');
+});
+
+router.get('/u/{user}', isLoggedIn, function(req, res) {
+    res.render('user', {user: req.user});
+});
+
+router.get('/u/{user}/{item}', isLoggedIn, function(req, res) {
+    res.render('userItem', { user: req.user}); 
+});
+
+router.get('/public', function(req, res) {
+    res.render('public');
+});
+
+router.get('/public/{item}', function(req, res) {
+    res.render('publicItem');
 });
 
 module.exports = router;
