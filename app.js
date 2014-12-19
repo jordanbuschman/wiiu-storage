9e81d1b4fa28f4d4e9f1557d8cdb94d0d5e0929f
Jordan Buschman <jordan.buschman@me.com>
2014-12-18 20:39:55 -0800

    jade->ejs

--- app.js
+++ app.js
@@ -4,14 +4,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var engine = require('ejs-locals');
 
 var routes = require('./routes/index');
 
 var app = express();
 
 // view engine setup
+app.engine('ejs', engine);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
--- routes/index.js
+++ routes/index.js
@@ -5,27 +5,42 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    res.render('index');
+    res.render('index', {title: 'Wii-U', what: 'best'});
 });
 
 router.get('/login', function(req, res) {
-    res.render('login');
+    res.render('login', {title: 'Wii-U - Login'});
 });
 
-router.get('/u/{user}', isLoggedIn, function(req, res) {
-    res.render('user', {user: req.user});
+router.get('/u/:user', isLoggedIn, function(req, res) {
+    res.render('user', {
+        title: 'Wii-U - ' + req.params.user + '\'s Files',
+        sessionUser: req.user,
+        user: req.param.user
+    });
 });
 
-router.get('/u/{user}/{item}', isLoggedIn, function(req, res) {
-    res.render('userItem', { user: req.user}); 
+router.get('/u/:user/:file', isLoggedIn, function(req, res) {
+    res.render('userItem', {
+        title: 'Wii-U - ' + req.params.file,
+        sessionUser: req.user,
+        user: req.param.user
+    }); 
 });
 
 router.get('/public', function(req, res) {
-    res.render('public');
+    res.render('public', {title: 'Wii-U - Public Files'});
 });
 
-router.get('/public/{item}', function(req, res) {
-    res.render('publicItem');
+router.get('/public/:file', function(req, res) {
+    res.render('publicItem', {title: 'Wii-U - ' + req.params.file});
 });
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated())
+        return next();
+
+    res.redirect('/');
+}
 
 module.exports = router;
