36f7731b6b6b3c0ff1879c1c1ee17266915f9441
Jordan Buschman <jordan.buschman@me.com>
2014-12-19 23:52:12 -0800

    DB.js->DBConn.js

---
+++ DBConn.js
@@ -0,0 +1,17 @@
+var bcrypt = require('bcrypt-nodejs');
+function DBConn() {
+    this.authenticateUser = function(username, password) {
+        if (!username || !password)
+            return false;
+
+        global.DB.User.find({ username: username }) 
+            .success(function(row) { //Found username, now get salt
+                console.log(row);
+                return true;
+            });
+            .failure(function() {
+                return false;
+            });
+    };
+};
+module.exports = DBConn;
