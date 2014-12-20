32aac05af292d823f68c31a7bf4347a13b600e32
Jordan Buschman <jordan.buschman@me.com>
2014-12-19 18:30:55 -0800

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,3 +1,16 @@
+-- init.sql --
+
+/*
+Run on local database to set it up.
+ONLY DO SO IF YOU ARE SURE YOU WANT TO CLEAR EVERYTHING
+
+To execute, go to terminal and type:
+    psql -h localhost -U postgres < init.sql
+*/
+
+CREATE DATABASE IF NOT EXISTS wiiu-storage;
+\c wiiu-storage
+ 
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
